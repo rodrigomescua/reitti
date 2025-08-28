f3f06b2ab063b72e567ba470c1a0184ed2f59d64
Daniel Graf <daniel@dedicatedcode.com>
2025-08-28 05:13:42 +0200

    fixed handling prefilled oidc configuration (#170)

--- src/main/java/com/dedicatedcode/reitti/ReittiApplication.java
+++ src/main/java/com/dedicatedcode/reitti/ReittiApplication.java
@@ -2,9 +2,10 @@ package com.dedicatedcode.reitti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { OAuth2ClientAutoConfiguration.class })
 @EnableAsync
 public class ReittiApplication {
 
--- src/main/java/com/dedicatedcode/reitti/config/OidcSecurityConfiguration.java
+++ src/main/java/com/dedicatedcode/reitti/config/OidcSecurityConfiguration.java
@@ -1,17 +1,33 @@
 package com.dedicatedcode.reitti.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesMapper;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @ConditionalOnProperty(name = "reitti.security.oidc.enabled", havingValue = "true")
+@EnableConfigurationProperties(OAuth2ClientProperties.class)
 public class OidcSecurityConfiguration {
+
+  @Bean
+  InMemoryClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties properties) {
+    List<ClientRegistration> registrations = new ArrayList<>(
+            new OAuth2ClientPropertiesMapper(properties).asClientRegistrations().values());
+    return new InMemoryClientRegistrationRepository(registrations);
+  }
 
   @Bean
   public OAuth2LoginConfigurer<HttpSecurity> oauth2LoginConfigurer(
